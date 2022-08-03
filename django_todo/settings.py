c53b4fc68e9cc9249c08799a82561869c40cb396
Flow-matic <jonathonvinson22@gmail.com>
2022-08-03 21:37:43 +0000

    added env.py file

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u=7&x#nh!6fxher0us&&^!=i5i6nrj^%e7*3it)g6g0-%20pe+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
